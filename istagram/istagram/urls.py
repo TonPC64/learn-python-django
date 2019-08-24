b79218b34b9f87f31fff04d7f9a66e95b00ce158
Chanwit Piromplad <chanwit.p@linecorp.com>
2019-08-24 17:08:36 +0700

    add profile page from custom

--- istagram/istagram/urls.py
+++ istagram/istagram/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', home, name="home"),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
+    path('profile/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- istagram/users/views.py
+++ istagram/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import User
+
+class ProfileDetailView(DetailView):
+    model = User
+
+    def get_object(self):
+        return get_object_or_404(User, username=self.kwargs.get('username'))
