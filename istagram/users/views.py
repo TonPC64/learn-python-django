261cb592f6a7fb935aa3050c299cb4e150a54596
Chanwit Piromplad <chanwit.p@linecorp.com>
2019-08-24 17:37:38 +0700

    update profile with own data

--- istagram/users/views.py
+++ istagram/users/views.py
@@ -2,9 +2,15 @@ from django.views.generic import DetailView
 from django.shortcuts import get_object_or_404
 
 from .models import User
+from posts.models import Post
 
 class ProfileDetailView(DetailView):
     model = User
 
     def get_object(self):
         return get_object_or_404(User, username=self.kwargs.get('username'))
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(**kwargs)
+        # context["posts"] = Post.objects.all()
+        return context
